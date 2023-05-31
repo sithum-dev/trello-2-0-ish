7a3d910de7829df6246e3495847659a735651d71
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2023-05-31 21:21:03 +0530

    Remove card func

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,4 @@
+import { useBoardStore } from "@/store/BoardStore";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDragHandleProps,
@@ -21,6 +22,8 @@ function TodoCard({
   draggableProps,
   dragHandleProps,
 }: Props) {
+  const deleteTask = useBoardStore((state) => state.deleteTasks);
+
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -30,7 +33,10 @@ function TodoCard({
     >
       <div className="flex justify-between items-center p-5">
         <p>{todo.title}</p>
-        <button className="text-red-500 hover:text-red-600">
+        <button
+          onClick={() => deleteTask(index, todo, id)}
+          className="text-red-500 hover:text-red-600"
+        >
           <XCircleIcon className="ml-5 h-8 w-8 " />
         </button>
       </div>
